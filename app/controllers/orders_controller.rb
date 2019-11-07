18f5f5ee2ea0ed099104ea1fcffeefb834587269
Avery Gay <agay12@uncc.edu>
2019-11-07 18:43:48 +0000

    resolved revert conflicts

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,7 @@
 class OrdersController < ApplicationController
     def new
     end
-<<<<<<< HEAD
 
-     def create
-         @order = Order.new(order_params)
-         
-         @order.save 
-         redirect_to @order
-     end
-     
-     def show
-                  @order = Order.find(params[:id])
-     end
-
-=======
->>>>>>> parent of 2bbfc4c... Set up create action
 end 
 
     private
