c5806505608431d83740c23e65a7f6fe9185d84d
Habukar <ec2-user@ip-172-31-22-199.us-east-2.compute.internal>
2019-11-11 23:53:03 +0000

    Listing orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,6 +13,10 @@ class OrdersController < ApplicationController
     def show 
         @order = Order.find(params[:id])
     end
+    
+    def index
+        @orders = Order.all
+    end
 end
 
 
