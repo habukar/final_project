f25c20f269ec2fcbfac17f7d3738710046fdbb1a
Habukar <habukar1@uncc.edu>
2019-11-14 16:27:23 +0000

    Add reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   
-  resources :orders
+  resources :orders do
+    resources :reviews
+  end
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
